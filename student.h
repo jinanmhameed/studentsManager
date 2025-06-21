1ae0dd1321d0d83a408706d096d14da057ea53ae
Jinan <147433666+jinanmhameed@users.noreply.github.com>
2025-06-21 15:46:32 +0300

    Update student.h

--- student.h
+++ student.h
@@ -1,1 +1,20 @@
+#include <string>
+#include <vector>
 
+using std::string;
+
+class student{
+
+    private:
+    string id;
+    string name;
+    std::vector<int> scores;
+
+    public:
+    student(string id, string name);
+    void addGrade(int grade);
+    double getAverage() const;
+    void print() const;
+    string getID() const;
+    string getName() const;
+};
