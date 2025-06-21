2985feb5d78f60193781784fa24809bb7f415649
Jinan <147433666+jinanmhameed@users.noreply.github.com>
2025-06-21 15:47:08 +0300

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,1 +1,36 @@
+#include "student.h"
+#include <iostream>
+#include <numeric>
 
+student::student(string id, string name)
+{
+    this->id = id;
+    this->name = name;
+}
+void student::addGrade(int grade)
+{
+    this->scores.push_back(grade);
+}
+
+double student::getAverage() const
+{
+    double sum = 0;
+    for (int i = 0; i <= this->scores.size() - 1; i++)
+    {
+        sum += this->scores[i];
+    }
+    return sum / this->scores.size();
+}
+
+void student::print() const{
+    std::cout << "ID: " << this->id << std::endl;
+    std::cout << "Name: " << this->name << std::endl;
+    std::cout << "Average: " << this->getAverage() << std::endl;
+}
+
+string student::getID() const{
+    return this->id;
+}
+string student::getName() const{
+    return this->name;
+}
