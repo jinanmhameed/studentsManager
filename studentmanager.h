cb485dea8850bcbb66bab49fdc469a50f26a2595
Jinan <147433666+jinanmhameed@users.noreply.github.com>
2025-06-21 16:21:57 +0300

    Update studentmanager.h

--- studentmanager.h
+++ studentmanager.h
@@ -1,1 +1,15 @@
+#include "student.h"
+#include <vector>
 
+class studentManager
+{
+    private:
+        std::vector<student> students;
+    public:
+    void addStudent(const student& student);
+    void removeStudent(std::string id);
+    void printAll() const;
+    student* findStudent(std::string id);
+    void saveToFile(std::string filename) const;
+    void loadFromFile(std::string filename);
+};
